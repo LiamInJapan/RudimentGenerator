a90287669692aa74905708bfd9204597f393268e
Liam Conroy <mail@liam-conroy.com>
2015-08-04 00:27:50 +0900

    some triplication of beetz and implementation of single stroke four

--- generator.py
+++ generator.py
@@ -19,29 +19,67 @@ class RudimentGenerator:
 		True : midi.G_3+1,
 	}
 
+	beat_values = {
+		"1"   : 128,
+		"1/2" : 128/2,
+		"1/4" : 128/4,
+		"1/4t" : ((128/4)*4)/3,
+		"1/8" : 128/8,
+		"1/8t" : ((128/4)*4)/9,
+		"1/16" : 128/16,
+		"1/16t" : ((128/4)*4)/27,
+		"1/32" : 128/32,
+		"1/4t" : ((128/4)*4)/81,
+		"1/64" : 128/64,
+		"1/4t" : ((128/4)*4)/243,
+	}
+
+	one_beat_value = 256
+
+	beat_values_new = {
+		"1"     : (1,1),
+		"1/2"   : (1,2),
+		"1/3"   : (1,3),
+		"1/4"   : (1,4),
+		"1/6"   : (1,6),
+		"1/8"   : (1,8),
+		"1/12"  : (1,12),
+		"1/16"  : (1,16),
+		"1/24"  : (1,24),
+		"1/32"  : (1,32),
+		"1/48"  : (1,48),
+		"1/64"  : (1,64),
+		"1/96"  : (1,96),
+	}
+
 	left_stick = False
 
 	def single_stroke_roll(self):
 
 		for beat in range(0,4):
-			on = midi.NoteOnEvent(tick=0, velocity=120, pitch=self.sticking[self.left_stick])
+			on = midi.NoteOnEvent(tick=self.rest, velocity=120, pitch=self.sticking[self.left_stick])
 			self.track.append(on)
-			off = midi.NoteOffEvent(tick=100, pitch=self.sticking[self.left_stick])
+			next_beat = self.one_beat_value/self.beat_values_new["1/4"][1]
+			off = midi.NoteOffEvent(tick=next_beat, pitch=self.sticking[self.left_stick])
 			self.track.append(off)
 			self.left_stick = not self.left_stick
 
-	# TODO: Triplify this
 	def single_stroke_four(self):
 
-		for beat in range(0,4):
-			on = midi.NoteOnEvent(tick=100+self.offset, velocity=120, pitch=self.sticking[self.left_stick])
-			self.track.append(on)
-			off = midi.NoteOffEvent(tick=100, pitch=self.sticking[self.left_stick])
-			self.track.append(off)
-			self.left_stick = not self.left_stick
-			self.offset = 0
+		for beat in range(0,6):
+			if(beat < 4):
+				on = midi.NoteOnEvent(tick=self.rest, velocity=120, pitch=self.sticking[self.left_stick])
+				self.track.append(on)
+				self.rest = 0
+				next_beat = self.one_beat_value/self.beat_values_new["1/6"][1]
+				off = midi.NoteOffEvent(tick=next_beat, pitch=self.sticking[self.left_stick])
+				self.track.append(off)
+				self.left_stick = not self.left_stick
+				self.offset = 0
+			else:
+				self.rest += self.one_beat_value/self.beat_values_new["1/6"][1]
 
-		self.offset = 100
+		#self.offset = self.beat_values["1/4"]
 
 	def single_stroke_seven(self):
 
@@ -57,10 +95,11 @@ class RudimentGenerator:
 
 	def __init__(self, bpm):
 		
+		print "BPM: %d" % bpm
 		self.pattern = midi.Pattern()
 		self.track = midi.Track()
 		self.pattern.append(self.track)
-		self.offset = 0
+		self.rest = 0
 		tempo = midi.SetTempoEvent()
 		tempo.set_bpm(bpm)
 		self.track.append(tempo)
@@ -96,7 +135,7 @@ parser.add_argument("--reverse_sticking", help="reverse the sticking",
                     action="store_true")
 args = parser.parse_args()
 
-rg = RudimentGenerator(120)
+rg = RudimentGenerator(args.bpm)
 
 if args.reverse_sticking:
 	print "reverse sticking"
